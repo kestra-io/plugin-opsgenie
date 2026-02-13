f4284458cec06eb734fe25a57514eb32da99f572
François Delbrayelle <fdelbrayelle@gmail.com>
2026-02-13 08:30:21 +0100

    chore: update plugin categories (#9)

--- src/main/java/io/kestra/plugin/opsgenie/package-info.java
+++ src/main/java/io/kestra/plugin/opsgenie/package-info.java
@@ -1,6 +1,6 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for Opsgenie alert.",
-    categories = PluginSubGroup.PluginCategory.ALERTING
+    categories = PluginSubGroup.PluginCategory.BUSINESS
 )
 package io.kestra.plugin.opsgenie;
 
